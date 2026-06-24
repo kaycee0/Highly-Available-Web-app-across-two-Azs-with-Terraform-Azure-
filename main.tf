3542a5e8a8bce26a6d013b34e530490176aecf63
Kenechukwu Eloji <kenechukwu.eloji@Kenechukwus-MacBook-Pro.local>
2026-06-24 23:04:32 +0100

    Added Azure monitor autoscaling

--- main.tf
+++ main.tf
@@ -372,3 +372,62 @@ resource "azurerm_linux_virtual_machine_scale_set" "app" {
   }
 
 }
+
+### Auto Scaling (Azure Monitor Autoscale)
+
+resource "azurerm_monitor_autoscale_setting" "app" {
+  name                = "${var.project_name}-autoscale"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  target_resource_id  = azurerm_linux_virtual_machine_scale_set.app.id
+
+  profile {
+    name = "cpu-autoscale-profile"
+
+    capacity {
+      default = 4
+      minimum = 2
+      maximum = 5
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_linux_virtual_machine_scale_set.app.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 70  # Scale out above 70% CPU
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT5M"
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_linux_virtual_machine_scale_set.app.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 30  # Scale in below 30% CPU
+      }
+
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT5M"
+      }
+    }
+  }
+}
